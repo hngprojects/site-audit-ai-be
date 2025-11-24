5b331d5025afb5cabd64b5853674b204b7c673b0
Oluwagbemiga <taiwoemmanuel15@gmail.com>
2025-11-24 17:46:42 +0000

    chore(extractor): remove unused ExtractorResponse schema

--- app/features/page_extractor/schemas/extractor_schema.py
+++ app/features/page_extractor/schemas/extractor_schema.py
@@ -4,7 +4,3 @@ from pydantic import BaseModel, HttpUrl
 class ExtractorRequest(BaseModel):
     url: HttpUrl
 
-class ExtractorResponse(BaseModel):
-    subdomains: List[str]
-    pages: List[str]
-    important_pages: List[str]
