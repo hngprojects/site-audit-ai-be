47c5dbfa018fd18603b96827873b04364955279d
Muizzyranking <oyebowaleabdulmuiz@gmail.com>
2025-12-06 15:55:07 +0100

    feat: dashboard endpoint for admin

--- app/features/admin/routes/__init__.py
+++ app/features/admin/routes/__init__.py
@@ -1,8 +1,11 @@
 from fastapi import APIRouter
+from app.features.admin.routes.auth import router as auth_router
+from app.features.admin.routes.dashboard import router as dashboard_router
 from app.features.admin.routes.share_message import router as share_message_router
 
 
 router = APIRouter(prefix="/admin", tags=["Admin"])
 
-
+router.include_router(auth_router)
+router.include_router(dashboard_router)
 router.include_router(share_message_router)
